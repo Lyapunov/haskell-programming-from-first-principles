82de8f3679e0dfaded4bf3e3453de988f156ca74
Lyapunov <vaskorichard@gmail.com>
2017-01-29 23:41:43 +0100

    Continuing.

---
+++ chapter_3/print2.hs
@@ -0,0 +1,16 @@
+-- print2.hs
+module Print2 where
+
+main::IO ()
+main = do
+  putStrLn "Hey diddle diddle,"
+  putStr "The cat and "
+  putStrLn "the fiddle,"
+  putStr "The cow jumped "
+  putStrLn "over the moon."
+  putStr "The little dog "
+  putStrLn "laughed, "
+  putStr "To see "
+  putStrLn "such sport,"
+  putStr "And the dish ran "
+  putStrLn "away with the spoon."
