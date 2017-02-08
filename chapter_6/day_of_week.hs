627fc9011932f79948690d5234eed8f2951e8ff5
Lyapunov <vaskorichard@gmail.com>
2017-02-08 15:46:15 +0100

    Continuing.

---
+++ chapter_6/day_of_week.hs
@@ -0,0 +1,23 @@
+module DayOfWeekDataType where
+
+data DayOfWeek =
+  Mon | Tue | Wed | Thu | Fri | Sat | Sun
+
+instance Eq DayOfWeek where
+  (==) Mon Mon   = True
+  (==) Tue Tue   = True
+  (==) Wed Wed   = True
+  (==) Thu Thu   = True
+  (==) Fri Fri   = True
+  (==) Sat Sat   = True
+  (==) Sun Sun   = True
+  (==) _ _       = False
+
+-- day of week and numerical day of month
+data Date =
+  Date DayOfWeek Int
+
+instance Eq Date where
+  (==) (Date weekday dayOfMonth)
+       (Date weekday' dayOfMonth') =
+    weekday == weekday' && dayOfMonth == dayOfMonth'
