aca276e2c5bc42fc66cffabd1fbdc797ff02a10f
Lyapunov <vaskorichard@gmail.com>
2017-02-12 01:34:27 +0100

    Continuing.

--- chapter_6/nada.hs
+++ chapter_6/nada.hs
@@ -2,6 +2,14 @@ module NadaType where
 
 newtype Nada = Nada Double deriving (Eq, Show)
 
+instance Num Nada where
+  (Nada x) + (Nada y) = Nada(x + y)
+  (Nada x) * (Nada y) = Nada(x * y)
+  abs (Nada x) = Nada( abs x )
+  signum (Nada x) = Nada(signum x)
+  fromInteger x = Nada( fromInteger x )
+  negate (Nada x) = Nada( negate x )
+
 instance Fractional Nada where
   (Nada x) / (Nada y) = Nada (x / y)
   recip (Nada n) = Nada (recip n)
