02f98e6f8f0065a5cab2c27125d745dd3ae67224
Lyapunov <vaskorichard@gmail.com>
2017-02-12 20:27:52 +0100

    Continuing.

--- chapter_7/case_first.hs
+++ chapter_7/case_first.hs
@@ -12,3 +12,20 @@ tellIsItPalindrome x =
     True -> "It is palindrome."
     False -> "No, it is not palindrome."
   where pali = ( x == reverse x )
+
+-- functionC x y = if (x > y) then x else y
+functionC :: Ord a => a -> a -> a
+functionC x y =
+  case x > y of
+    True -> x
+    False -> y
+
+-- ifEvenAdd2 x = if ( even x ) then x 2 else x
+ifEvenAdd2First :: Integral a => a -> a
+ifEvenAdd2First x = if ( even x ) then x + 2 else x
+
+ifEvenAdd2Second :: Integral a => a -> a
+ifEvenAdd2Second x =
+  case ( even x ) of
+    True  -> x + 2
+    False -> x
