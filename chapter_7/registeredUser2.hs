b5e7cf6ad49043700b9aee5d03a6a5573a461ffb
Lyapunov <vaskorichard@gmail.com>
2017-02-12 18:40:40 +0100

    Continuing.

---
+++ chapter_7/registeredUser2.hs
@@ -0,0 +1,18 @@
+module RegisteredUser where
+
+newtype Username = Username String deriving Show
+newtype AccountNumber = AccountNumber Integer deriving Show
+
+data User = UnregisteredUser
+          | RegisteredUser Username AccountNumber deriving Show
+
+-- > RegisteredUser (Username "Fucker") (AccountNumber 5) 
+-- RegisteredUser (Username "Fucker") (AccountNumber 5)
+
+printUser :: User -> IO ()
+printUser UnregisteredUser = putStrLn "UnregisteredUser"
+printUser ( RegisteredUser (Username name)
+                           (AccountNumber number) )
+          = putStrLn $ name ++ " " ++ show number
+
+-- > printUser ( RegisteredUser (Username "hello") (AccountNumber 23) )
