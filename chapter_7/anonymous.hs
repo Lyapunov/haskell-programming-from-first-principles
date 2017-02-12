4709208c74eb5359cd5c24a2da37eb40605a8834
Lyapunov <vaskorichard@gmail.com>
2017-02-12 14:02:06 +0100

    Continuing.

--- chapter_7/anonymous.hs
+++ chapter_7/anonymous.hs
@@ -6,3 +6,14 @@ triple1 x = 3 * x
 triple2 = (\x -> 3 * x) :: Integer -> Integer
 
 triple3 = (\x -> 3 * x)
+
+--- lambda pyramid
+mthi1 x y z = x * y * z
+mthi2 x y = \z -> x * y * z
+mthi3 x = \y -> \z -> x * y * z
+mthi4 = \x -> \y -> \z -> x * y * z
+
+addOneIfOdd n = case odd n of
+  True -> f n
+  False -> n
+  where f n = n + 1
