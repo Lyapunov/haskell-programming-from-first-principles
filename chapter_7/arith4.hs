a48eb1fe13f74a35fa4e2172c1560bbeae42869f
Lyapunov <vaskorichard@gmail.com>
2017-02-13 05:14:38 +0100

    Continuing.

--- chapter_7/arith4.hs
+++ chapter_7/arith4.hs
@@ -7,6 +7,10 @@ module Arith4 where
 roundTrip :: ( Show a, Read a ) => a -> a
 roundTrip a = read (show a)
 
+roundTripPF :: ( Show a, Read a ) => a -> a
+roundTripPF = read . show
+
 main = do
   print (roundTrip 4)
+  print (roundTripPF 4)
   print (id 4)
