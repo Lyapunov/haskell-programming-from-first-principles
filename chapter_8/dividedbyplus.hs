b99bd6fc1531f63f6177f0456f3101b5b9fb03eb
Lyapunov <vaskorichard@gmail.com>
2017-02-15 05:24:54 +0100

    Extending.

--- chapter_8/dividedbyplus.hs
+++ chapter_8/dividedbyplus.hs
@@ -3,14 +3,17 @@ module DividedByPlus where
 data DivisionResult a = Valid a a | Invalid deriving (Show, Eq)
                  
 dividedBy :: Integral a => a -> a -> DivisionResult a
-dividedBy num denom = go num denom 0
-  where go n   d count
-         | n >= 0 && d > 0 && n < d = Valid count n
-         | n <= 0 && d < 0 && n > d = Valid count n
-         | n >= d && d > 0 = go (n - d) d (count + 1)
-         | n <= d && d < 0 = go (n - d) d (count + 1)
-         | n > 0  && d < 0 = go (n + d) d (count - 1)
-         | n < 0  && d > 0 = go (n + d) d (count - 1)
-         | otherwise = Invalid
+dividedBy num denom
+    | denom > 0 = go_plus num denom 0
+    | denom < 0 = go_neg  num denom 0
+    | otherwise = Invalid
+  where go_plus n d count
+         | n >= 0 && n < d = Valid count n
+         | n >= d = go_plus (n - d) d (count + 1)
+         | n < 0  = go_plus (n + d) d (count - 1)
+        go_neg  n d count
+         | n <= 0 && n > d = Valid count n
+         | n <= d = go_neg (n - d) d (count + 1)
+         | n > 0  = go_neg (n + d) d (count - 1)
 
 -- eg.: (6, -3, 0) -> (3, -3, -1) -> (0, -3, 2)
