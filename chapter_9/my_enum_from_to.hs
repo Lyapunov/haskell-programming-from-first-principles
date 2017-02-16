362b748f5970570c94a89e003c965924fb566574
Lyapunov <vaskorichard@gmail.com>
2017-02-16 05:34:48 +0100

    Continuing.

--- chapter_9/my_enum_from_to.hs
+++ chapter_9/my_enum_from_to.hs
@@ -4,3 +4,34 @@ myEnumFromTo :: Enum a => a -> a -> [a]
 myEnumFromTo x y
  | fromEnum x > fromEnum y = []
  | otherwise = x : myEnumFromTo (succ x) y
+
+-- piece of cake
+eftBool :: Bool -> Bool -> [Bool]
+eftBool True False  = []
+eftBool True True   = [True]
+eftBool False True  = [False, True]
+eftBool False False = [False]
+
+-- just for fun
+eftOrd :: Ordering -> Ordering -> [Ordering]
+eftOrd GT EQ = []
+eftOrd GT LT = []
+eftOrd EQ LT = []
+eftOrd LT LT = [LT]
+eftOrd EQ EQ = [EQ]
+eftOrd GT GT = [GT]
+eftOrd LT EQ = [LT, EQ]
+eftOrd EQ GT = [EQ, GT]
+eftOrd LT GT = [LT, EQ, GT]
+
+-- ordering is easy for Int
+eftInt :: Int -> Int -> [Int]
+eftInt x y
+ | x > y = []
+ | otherwise = x : eftInt (succ x) y
+
+-- ordering is easy for Char as well
+eftChar :: Char -> Char -> [Char]
+eftChar x y
+ | x > y = []
+ | otherwise = x : eftChar (succ x) y
