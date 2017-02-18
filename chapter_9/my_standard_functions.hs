497f1bde885257e76988313bb601bc5b7bb3d5fe
Lyapunov <vaskorichard@gmail.com>
2017-02-18 23:54:43 +0100

    Continuing.

--- chapter_9/my_standard_functions.hs
+++ chapter_9/my_standard_functions.hs
@@ -3,3 +3,7 @@ module MyStandardFunctions where
 myOr :: [Bool] -> Bool
 myOr [] = False
 myOr (x:xs) = x || myOr xs
+
+myAny :: ( a -> Bool ) -> [a] -> Bool
+myAny f [] = False
+myAny f (x:xs) = f(x) || myAny f xs
