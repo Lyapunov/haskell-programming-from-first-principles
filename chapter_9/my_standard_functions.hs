6543ecb6265c1eb9b52699c89f8c048f661aa019
Lyapunov <vaskorichard@gmail.com>
2017-02-19 00:03:27 +0100

    Continuing.

--- chapter_9/my_standard_functions.hs
+++ chapter_9/my_standard_functions.hs
@@ -7,3 +7,10 @@ myOr (x:xs) = x || myOr xs
 myAny :: ( a -> Bool ) -> [a] -> Bool
 myAny f [] = False
 myAny f (x:xs) = f(x) || myAny f xs
+
+myElem1 :: Eq a => a -> [a] -> Bool
+myElem1 x (y:ys) = x == y || myElem1 x ys
+myElem1 x []     = False
+
+myElem2 :: Eq a => a -> [a] -> Bool
+myElem2 x = myAny ( (==) x )
