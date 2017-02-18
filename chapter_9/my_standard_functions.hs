c1112fbd783f765bae465e5ae9019ab0e4e1258d
Lyapunov <vaskorichard@gmail.com>
2017-02-19 00:54:48 +0100

    Continuing.

--- chapter_9/my_standard_functions.hs
+++ chapter_9/my_standard_functions.hs
@@ -12,6 +12,7 @@ myElem1 :: Eq a => a -> [a] -> Bool
 myElem1 x (y:ys) = x == y || myElem1 x ys
 myElem1 x []     = False
 
+-- myElem1 via using myAny
 myElem2 :: Eq a => a -> [a] -> Bool
 myElem2 x = myAny ( (==) x )
 
@@ -19,3 +20,27 @@ myReverse :: [a] -> [a]
 myReverse xl = go [] xl
    where go xl [] = xl
          go xl (y:ys) = go (y:xl) ys
+
+squish :: [[a]] -> [a]
+squish xl = go [] xl
+   where go xl [] = xl
+         go xl (y:ys) = go (xl ++ y) ys
+
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap _ [] = []
+squishMap f (x:xs) = f(x) ++ squishMap f xs
+
+squishMap2 :: (a -> [b]) -> [a] -> [b]
+squishMap2 f xl = go f xl []
+   where go _ [] yl  = yl
+         go f (x:xs) yl = go f xs (yl ++ f(x))
+
+-- squish via using squishMap
+squishAgain :: [[a]] -> [a]
+squishAgain = squishMap ( \x -> x )
+
+myMaximumBy :: ( a -> a -> Ordering ) -> [a] -> a
+myMaximumBy f (x:xs) = go x xs
+   where go x [] = x
+         go x (y:ys) = if f x y == LT then go y (ys) 
+                                       else go x (ys)
